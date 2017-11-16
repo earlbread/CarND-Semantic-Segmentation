7e10bd8a2a33b0040bc9bf22a1dccf0e3a3bfd88
Seunghun Lee <waydi1@gmail.com>
2017-11-16 22:06:06 +0900

    Implement optimize

--- main.py
+++ main.py
@@ -93,7 +93,12 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
     # TODO: Implement function
-    return None, None, None
+    logits = tf.reshape(nn_last_layer, (-1, num_classes))
+    entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=correct_label)
+    cross_entropy_loss = tf.reduce_mean(entropy)
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+    train_op = optimizer.minimize(cross_entropy_loss)
+    return logits, train_op, cross_entropy_loss
 tests.test_optimize(optimize)
 
 
